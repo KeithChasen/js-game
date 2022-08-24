47c720b9d0b39d415381d0521b70289b0bdc0c5b
kostiantyn-chernysh <kostiantyn.chernysh@nentgroup.com>
2022-08-24 19:45:16 +0300

    draw hero shadow

--- Overworld.js
+++ Overworld.js
@@ -14,6 +14,23 @@ class Overworld {
 
         const x = 1;
         const y = 4;
+
+        const shadow = new Image();
+        shadow.onload = () => {
+            this.ctx.drawImage(
+                shadow,
+                0,
+                0,
+                32,
+                32,
+                x * 16 - 8,
+                y * 16 - 18,
+                32,
+                32
+            )
+        };
+        shadow.src = "/images/characters/shadow.png";
+
         const hero = new Image();
         hero.onload = () => {
             this.ctx.drawImage(
