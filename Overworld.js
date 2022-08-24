0c36d6dd7794ef64feedc655bebd35f7a7c418b3
kostiantyn-chernysh <kostiantyn.chernysh@nentgroup.com>
2022-08-24 18:49:19 +0300

    drawing hero image / single frame of a spritesheet

--- Overworld.js
+++ Overworld.js
@@ -12,9 +12,21 @@ class Overworld {
         };
         image.src = "/images/maps/DemoLower.png";
 
+        const x = 0;
+        const y = 0;
         const hero = new Image();
         hero.onload = () => {
-            this.ctx.drawImage(hero, 0, 0)
+            this.ctx.drawImage(
+                hero,
+                0, // left start picture coordinate
+                0, // top start picture coordinate
+                32, // width to take
+                32, // height to take
+                x, // where to place image on canvas left
+                y, // where to place image on canvas top
+                32, // width of the image to use
+                32 // height of the image to use
+            )
         };
         hero.src = "/images/characters/people/hero.png";
     }
