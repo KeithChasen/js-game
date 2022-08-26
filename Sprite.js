65f3d6ce5bdf8e72a31cff609ce7298441862347
kostiantyn-chernysh <kostiantyn.chernysh@nentgroup.com>
2022-08-26 22:50:19 +0300

    set animation method to control animation direction

--- Sprite.js
+++ Sprite.js
@@ -18,22 +18,35 @@ class Sprite {
 
         // Configure Animation and Initial State
         this.animations = config.animations || {
-            'idle-down': [ [0,0] ],
-            'walk-down': [ [1,0], [0,0], [3,0], [0,0] ],
+            'idle-down':  [ [0,0] ],
+            'idle-right': [ [0,1] ],
+            'idle-up':    [ [0,2] ],
+            'idle-left':  [ [0,3] ],
+            'walk-down':  [ [1,0], [0,0], [3,0], [0,0] ],
+            'walk-right': [ [1,1], [0,1], [3,1], [0,1] ],
+            'walk-up':    [ [1,2], [0,2], [3,2], [0,2] ],
+            'walk-left':  [ [1,3], [0,3], [3,3], [0,3] ],
         }
-        // this.currentAnimation = config.currentAnimation || 'idle-down';
-        this.currentAnimation = 'walk-down';
+        this.currentAnimation = config.currentAnimation || 'idle-down';
         this.currentAnimationFrame = 0;
 
         // basically speed of changing frames
-        this.animationFrameLimit = config.animationFrameLimit || 16;
-        this.animationFrameProgress = config.animationFrameLimit || 16;
+        this.animationFrameLimit = config.animationFrameLimit || 8;
+        this.animationFrameProgress = config.animationFrameLimit;
 
         this.gameObject = config.gameObject;
     }
 
     get frame() {
         return this.animations[this.currentAnimation][this.currentAnimationFrame];
+    }
+
+    setAnimation(key) {
+        if (this.currentAnimation !== key) {
+            this.currentAnimation = key;
+            this.currentAnimationFrame = 0;
+            this.animationFrameProgress = this.animationFrameLimit;
+        }
     }
 
     updateAnimationProgress() {
